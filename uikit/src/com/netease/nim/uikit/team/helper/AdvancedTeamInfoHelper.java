4fc721290e00b23ab06e169304242b36a4237ac5
lidonglin90 <827211779@qq.com>
2020-05-14 14:43:56 +0800

    1

---
+++ uikit/src/com/netease/nim/uikit/team/helper/AdvancedTeamInfoHelper.java
@@ -0,0 +1,20 @@
+package com.netease.nim.uikit.team.helper;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.netease.nim.uikit.NimUIKit;
+import com.netease.nim.uikit.R;
+import com.netease.nimlib.sdk.NIMClient;
+import com.netease.nimlib.sdk.RequestCallback;
+import com.netease.nimlib.sdk.ResponseCode;
+import com.netease.nimlib.sdk.team.TeamService;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hzxuwen on 2016/9/21.
+ */
+public class AdvancedTeamInfoHelper {
+    private static final String TAG = AdvancedTeamInfoHelper.class.getSimpleName();
+}
